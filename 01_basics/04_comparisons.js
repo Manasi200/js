2c5db3928b1a94e4ff5bd46623cf7ce2df6c67f6
Manasi <74507247+Manasi200@users.noreply.github.com>
2024-07-12 12:27:28 +0000

    null, undefined when with operators

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// comparison and equality operators take null differently; apparently comparison operators take null as a number (0)
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict checking; checks datatype as well
+
+console.log("2" === 2);
